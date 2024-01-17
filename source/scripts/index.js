24c7d7b46baccd081b7d44538c3a45d04000bfd2
Максим <sterh84@gmail.com>
2024-01-18 00:19:56 +0300

    Добавляет preload fons

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navigation = document.querySelector('.main-header__navigation');
+const contactsIframe = document.querySelector('.contacts__iframe');
+const buttonBurger = document.querySelector('.main-header__button-toggle');
+const navMenu = document.querySelector('.main-header__navigation');
+
+navigation.classList.remove('main-header__navigation--no-js');
+contactsIframe.classList.remove('contacts__iframe--no-js');
+buttonBurger.classList.remove('main-header__button-toggle--no-js');
+
+//menu-mobile
+buttonBurger.onclick = function () {
+  buttonBurger.classList.toggle('main-header__button-toggle--open');
+  navMenu.classList.toggle('main-header__navigation--open');
+};
